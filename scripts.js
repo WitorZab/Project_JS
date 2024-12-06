c0c2fa8bcd38d76a0d12c17b12a560e439181dd1
WitorZab <wiktorzabawski@gmail.com>
2024-12-06 15:39:19 +0100

    Add files via upload
    hamburger

--- scripts.js
+++ scripts.js
@@ -22,22 +22,22 @@ function countdown(){
 
     var x = setInterval(function() {
 
-    var now = new Date().getTime();
-        
-    var distance = countDownDate - now;
-        
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+        var now = new Date().getTime();
+            
+        var distance = countDownDate - now;
+            
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "KLAAR";
-    }
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "KLAAR";
+        }
     }, 1000);
 }
 
@@ -48,3 +48,28 @@ function cookieBox(){
     document.querySelector(".blur-background").style.display = "none"
     console.log("cookies have been accepted/declined")
 }
+
+function hamburger() {
+    var x = document.getElementById("xd");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
+
+
+
+//   display: flex;
+//   position: absolute;
+//   justify-content: center;
+//   align-items: center;
+//   top: 13%;
+//   left: 30%;
+//   min-width: 700px;
+//   min-height: 700px;
+//   z-index: 1000;
+//   background-color: rgb(48, 48, 48);
+//   color: aliceblue;
+//   border: #3d9bf8 solid;
+//   border-radius: 5px;
